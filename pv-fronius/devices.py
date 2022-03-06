586a96b29271b0da93d2335a8c6dec12c575090a
ThomasRgbg <ThomasRgbg@users.noreply.github.com>
2022-03-06 12:54:22 +0100

    devices.py with default values

---
+++ pv-fronius/devices.py
@@ -0,0 +1,26 @@
+
+#
+# Device Database and registry
+#
+
+
+# Need to be adapted to your local devices.
+# And these are just default values, not my ones... :)
+
+mqtt_ip = "192.168.178.10"
+mqtt_port = 1883
+
+influxdb_ip = "192.168.178.12"
+influxdb_user = "myuser"
+influxdb_pw = "mypw"
+influxdb_db = "mydb"
+
+fritz_ip = "192.168.17.1"
+fritz_user = "fritzuser"
+fritz_pw = "fritzpasswd"
+fritz_evswitch = "11234 0134134"
+
+gen24_ip = "192.168.178.13"
+
+ipump_ip = "192.168.178.14"
+
